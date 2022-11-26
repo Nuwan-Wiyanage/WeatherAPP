979d8703c1e193d63c5bc8b99df13f48636d25b8
Nuwan-Wiyanage <nwiyanage@gmail.com>
2022-11-26 17:22:25 +0530

    Migrate to scss

--- src/app/home/components/weather-report-today/weather-report-today.component.ts
+++ src/app/home/components/weather-report-today/weather-report-today.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-weather-report-today',
   templateUrl: './weather-report-today.component.html',
-  styleUrls: ['./weather-report-today.component.css']
+  styleUrls: ['./weather-report-today.component.scss']
 })
 export class WeatherReportTodayComponent implements OnInit {
 
--- src/app/home/components/weather-report-weekly/weather-report-weekly.component.ts
+++ src/app/home/components/weather-report-weekly/weather-report-weekly.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-weather-report-weekly',
   templateUrl: './weather-report-weekly.component.html',
-  styleUrls: ['./weather-report-weekly.component.css']
+  styleUrls: ['./weather-report-weekly.component.scss']
 })
 export class WeatherReportWeeklyComponent implements OnInit {
 
